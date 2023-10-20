310c5c8020f98cb3f3ab27330b65188b289d8257
romrobluk <127985848+romrobluk@users.noreply.github.com>
2023-10-20 21:37:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 function add_bot() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const ccode = urlParams.get('code');
   window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1164253855102545961&permissions=8&redirect_uri=https%3A%2F%2Fdarkbot-ten.vercel.app&response_type=code&scope=bot%20identify");
 }
