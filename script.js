69b9dad8508ebbfba59718226ace1e8f5195bbed
romrobluk <127985848+romrobluk@users.noreply.github.com>
2023-10-20 21:15:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function add_bot() {
-  window.open("https://discord.com/api/oauth2/authorize?client_id=1164253855102545961&permissions=8&scope=bot")
+  window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1164253855102545961&permissions=8&scope=bot");
 }
