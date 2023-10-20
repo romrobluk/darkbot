6e287656ebded365ec9b0b8da9ea292a792b37a6
romrobluk <127985848+romrobluk@users.noreply.github.com>
2023-10-20 21:43:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add_bot() {
-  window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1164253855102545961&permissions=8&redirect_uri=https%3A%2F%2Fdarkbot-ten.vercel.app&response_type=code&scope=bot%20identify");
+  window.location.replace("https://discord.com/api/oauth2/authorize?client_id=1164253855102545961&permissions=8&redirect_uri=https%3A%2F%2Fdarkbot-ten.vercel.app%2Fauthorized.html&response_type=code&scope=bot%20identify");
 }
 
 function get_code() {
